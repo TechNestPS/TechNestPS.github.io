f8340c81cf23565f394e99d8d081c401a877e619
TechNestPS <mahmoud.kanh13@gmail.com>
2025-10-04 18:42:49 +0300

    script.js

--- SheryBeautyCenter/script.js
+++ SheryBeautyCenter/script.js
@@ -1,1 +1,18 @@
+document.getElementById("joinForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let address = document.getElementById("address").value;
+  let phone = document.getElementById("phone").value;
+  let age = document.getElementById("age").value;
+
+  let message = `مرحباً، أود الانضمام إلى الدورة/التدريب:
+  👩‍💼 الاسم: ${name}
+  📍 العنوان: ${address}
+  📞 الهاتف: ${phone}
+  🎂 العمر: ${age}`;
+
+  let url = `https://wa.me/970597889681?text=${encodeURIComponent(message)}`;
+  window.open(url, "_blank");
+});
 
